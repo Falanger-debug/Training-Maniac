e4e3027d9168fa415c632b3c27a5e961428143e6
Falanger-debug <falanger27@gmail.com>
2024-10-14 18:51:28 +0200

    added starting files

---
+++ src/main/java/com/Training_Maniac/TrainingManiacApplication.java
@@ -0,0 +1,13 @@
+package com.Training_Maniac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingManiacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingManiacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Training_Maniac/TrainingManiacApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Training_Maniac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingManiacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
